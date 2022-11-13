fabe686374fa8fb416ad47db88167528a261d675
SanskarPoudel <72004392+SanskarPoudel@users.noreply.github.com>
2022-11-13 23:17:15 +0545

    Assignment Day 12

---
+++ Day 12 Assignment/script.js
@@ -0,0 +1,43 @@
+// First Assignment
+
+const fizzBuzzAssignment = (number)=> {
+  let resultArr=[];
+
+  for(let i=1;i<=number;i++){
+
+    if(i%3===0 && i%5=== 0){
+      resultArr.push("FizzBuzz");
+ }
+
+    else if(i%3===0){
+      resultArr.push('Fizz');
+    }
+
+    else if(i%5===0){
+          resultArr.push("Buzz")    
+    }
+
+    else{
+          resultArr.push(i.toString());
+    }
+  }
+  console.log(resultArr);
+}
+
+fizzBuzzAssignment(60);
+
+
+
+//Second Assignment
+
+let inputArr=[];
+const result={};
+const arr = (inputArr) =>{
+   inputArr.forEach(element => {
+    result[element] = (result[element] || 0) + 1;
+   });
+}
+arr([1,2,'Sushil',1,2,'Sushil','Sushil'])
+console.log(result) ; 
+
+
