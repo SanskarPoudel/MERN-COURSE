d5d3b34149af83a48144e458b80974bdf69768df
SanskarPoudel <72004392+SanskarPoudel@users.noreply.github.com>
2022-11-15 11:10:54 +0545

    small change

--- reactapp/src/components/counter/Counter.js
+++ reactapp/src/components/counter/Counter.js
@@ -18,9 +18,9 @@ export default function Counter() {
 
   return (
     <div className=' counter h-100 d-flex align-items-center justify-content-center'>
-      <button type="button" class="btn btn-primary mt-2 mx-2" onClick={handleMinus} >Decrease(-)</button>
+      <button type="button" className="btn btn-primary mt-2 mx-2 mb-2" onClick={handleMinus} >Decrease</button>
      <h4>{count}</h4>
-      <button type="button" class="btn btn-primary mt-2 mx-2" onClick={handlePlus}>Increase(+)</button>
+      <button type="button" className="btn btn-primary mt-2 mx-2 mb-2" onClick={handlePlus}>Increase</button>
 
     </div>
   )
